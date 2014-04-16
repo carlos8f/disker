31ad79046147bd1fb02653612fd87479c03da388
Carlos Rodriguez <carlos@s8f.org>
2014-04-16 11:25:20 -0700

    hashing a stream

--- test/crypto.js
+++ test/crypto.js
@@ -6,4 +6,23 @@ describe('crypto', function () {
     var compare = require('crypto').createHash('sha256').update('carlos').digest('hex');
     assert.strictEqual(Buffer(hash, 'base64').toString('hex'), compare);
   });
+  it('hashes a stream', function (done) {
+    var stream = kafs.crypto.hashStream();
+    var equal = false;
+    stream.on('data', function (data) {
+      assert.equal(data, 'e4UXW0VQYOMjfpJfAjBTypUV6Ggqg8iwmRHHJKH4t18');
+      equal = true;
+    });
+    stream.on('end', function () {
+      assert(equal);
+      done();
+    });
+    setImmediate(function () {
+      stream.write('ca');
+      setImmediate(function () {
+        stream.write('rlos');
+        stream.end();
+      });
+    });
+  });
 });
